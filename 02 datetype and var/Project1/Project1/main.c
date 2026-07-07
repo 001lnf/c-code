b1d4efa7e0dda1aab9865a2f033e32922f491341
001lnf <3161000577@qq.com>
2026-07-07 18:23:12 +0800

    02

---
+++ 02 datetype and var/Project1/Project1/main.c
@@ -0,0 +1,14 @@
+//数据类型分为内置类型和自定义类型，内置就是自带的，有字符，整数，浮点数，布尔
+//自定义类型有数组，结构体，联合体，枚举
+//首先看字符型，有char，signed char，unsigned char
+
+/*整型有许多种，分为短整型，short int ，signed short  int，unsigned short  int
+整型 int ， signed int， unsigned int
+长整型long int ，signed long  int，unsigned long  int
+长长整型long long int，signed long long int，unsigned long long int
+
+浮点型 float double long double
+布尔类型_Bool,需要包含头文件stdbool
+
+*/
+
